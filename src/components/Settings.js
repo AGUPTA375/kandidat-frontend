c9e5cc061264f21128f56bae85acf65fcff2b3fc
Rohat Bedikanli <rohatretki@icloud.com>
2022-04-22 14:56:30 +0200

    Update Settings.js

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
         backgroundColor:"white",
         borderColor: "black",
         borderWidth: 1,
-        borderRadius: 10,
+        borderRadius: 40,
         alignSelf:"center",
         marginTop: "20%",
         shadowColor: "#000",
